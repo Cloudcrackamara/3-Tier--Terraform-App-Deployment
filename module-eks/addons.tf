0b25403cd2639e6d38ec2f5ec08cb19c2a2d2e14
Kennedy <kenedynwachukwu@gmail.com>
2025-12-04 20:39:54 +0100

    updated code

--- module-eks/addons.tf
+++ module-eks/addons.tf
@@ -67,10 +67,6 @@ resource "helm_release" "nginx_ingress" {
 data "aws_lb" "nginx_ingress" {
   depends_on = [helm_release.nginx_ingress]
 
-#   tags = {
-#     "kubernetes.io/service-name" = "ingress-nginx/ingress-nginx-controller"
-#   }
-# }
   tags = {
     "kubernetes.io/service-name" = "ingress-nginx/ingress-nginx-controller"
   }
