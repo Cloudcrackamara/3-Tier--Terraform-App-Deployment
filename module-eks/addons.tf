ab692505d5e6c368669be00cd23cc9cd22b3fdd2
Rita <ugwuanyinnenna43@gmail.com>
2025-12-04 19:21:37 +0000

    addon

--- module-eks/addons.tf
+++ module-eks/addons.tf
@@ -40,23 +40,19 @@ resource "helm_release" "nginx_ingress" {
   name      = "nginx-ingress"
   namespace = "ingress-nginx"
 
-  # Dummy chart info (Terraform does NOT install anything)
   repository = "https://kubernetes.github.io/ingress-nginx"
-  chart      = "ingress-nginx"
+  chart      = "nginx-ingress"
   version    = "4.12.0"
 
-  #  KEY FIX — tell Terraform to LEAVE IT ALONE completely
   lifecycle {
     ignore_changes = all
     prevent_destroy = true
   }
 
-  #  Do NOT wait, do NOT validate, do NOT check cluster
   timeout          = 1
   disable_webhooks = true
   recreate_pods    = false
 
-  # This ensures Terraform DOES NOT try to install or modify it
   depends_on = []
 }
 
@@ -67,10 +63,6 @@ resource "helm_release" "nginx_ingress" {
 data "aws_lb" "nginx_ingress" {
   depends_on = [helm_release.nginx_ingress]
 
-#   tags = {
-#     "kubernetes.io/service-name" = "ingress-nginx/ingress-nginx-controller"
-#   }
-# }
   tags = {
     "kubernetes.io/service-name" = "ingress-nginx/ingress-nginx-controller"
   }
