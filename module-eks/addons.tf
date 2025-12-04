621e12c692dfa9fd406f59bbe63647f4eeb60d35
Rita <ugwuanyinnenna43@gmail.com>
2025-12-04 18:18:31 +0000

    addon

--- module-eks/addons.tf
+++ module-eks/addons.tf
@@ -21,7 +21,7 @@ provider "kubernetes" {
 }
 
 ############################################
-# HELM PROVIDER (currently unused, but harmless)
+# HELM PROVIDER
 ############################################
 
 provider "helm" {
@@ -33,12 +33,51 @@ provider "helm" {
 }
 
 ############################################
+# 🔥 FINAL FIX:
+# Let Terraform KNOW the release exists,
+# but DO NOT allow Terraform to install or update it.
+############################################
+
+resource "helm_release" "nginx_ingress" {
+  name       = "nginx-ingress"
+  namespace  = "ingress-nginx"
+  chart      = "ingress-nginx"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  version    = "4.7.1"
+
+  # Do NOT install because it already exists
+  create_namespace = false
+  timeout          = 1200
+
+  # This prevents Terraform from updating or reinstalling it
+  lifecycle {
+    ignore_changes = [
+      chart,
+      version,
+      values,
+      repository,
+    ]
+  }
+
+  # Dummy values so Terraform is satisfied
+  values = [
+    <<EOF
+controller:
+  allowSnippetAnnotations: true
+EOF
+  ]
+}
+
+
+############################################
 # DISCOVER EXISTING NGINX LOAD BALANCER
-# (ingress controller was installed manually)
 ############################################
 
 data "aws_lb" "nginx_ingress" {
+  depends_on = [helm_release.nginx_ingress]
+
   tags = {
     "kubernetes.io/service-name" = "ingress-nginx/ingress-nginx-controller"
   }
 }
+
